ad6b29279c6761852ce3b085b683a5ef02f6eee1
fairy-scary <ecoautumn@gmail.com>
2021-01-11 17:54:11 -0600

    refactor

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,16 +1,28 @@
 def parse(string):
-    if string == "I":
-        return 1
-    elif string == "II":
-        return 2
-    elif string == "III":
-        return 3
-    elif string == "IV":
-        return 4
-    elif string == "V":
-        return 5
-    elif string == "VI":
-        return 6
-    elif string == "VII":
-        return 7
-    
+
+    values = []
+    for char in string:
+        if char == "I":
+            values.append(1)
+        elif char == "V":
+            values.append(5)
+        elif char == "X":
+            values.append(10)
+
+
+    # if string == "I":
+    #     return 1
+    # elif string == "II":
+    #     return 2
+    # elif string == "III":
+    #     return 3
+    # elif string == "IV":
+    #     return 4
+    # elif string == "V":
+    #     return 5
+    # elif string == "VI":
+    #     return 6
+    # elif string == "VII":
+    #     return 7
+    # elif string == "VIII":
+    #     return 9
